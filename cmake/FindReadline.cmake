af412088f0cf780db961b55b3607d4254cfdbbe3
Filipe <filipefalcaosantos@gmail.com>
2019-11-20 12:03:19 -0300

    [refact] Minor improvements to the CMake build.

--- cmake/FindReadline.cmake
+++ cmake/FindReadline.cmake
@@ -1,21 +1,21 @@
 # Search for the path containing library's headers
-find_path(Readline_ROOT_DIR NAMES include/readline/readline.h)
+find_path(READLINE_ROOT_DIR NAMES include/readline/readline.h)
 
 # Search for include directory
-find_path(Readline_INCLUDE_DIR NAMES readline/readline.h HINTS ${Readline_ROOT_DIR}/include)
+find_path(READLINE_INCLUDE_DIR NAMES readline/readline.h HINTS ${READLINE_ROOT_DIR}/include)
 
 # Search for library
-find_library(Readline_LIBRARY NAMES readline HINTS ${Readline_ROOT_DIR}/lib)
+find_library(READLINE_LIBRARY NAMES readline HINTS ${READLINE_ROOT_DIR}/lib)
 
 # Conditionally set READLINE_FOUND value
-if(Readline_INCLUDE_DIR AND Readline_LIBRARY AND Ncurses_LIBRARY)
+if(READLINE_INCLUDE_DIR AND READLINE_LIBRARY AND NCURSES_LIBRARY)
     set(READLINE_FOUND TRUE)
-else(Readline_INCLUDE_DIR AND Readline_LIBRARY AND Ncurses_LIBRARY)
-    FIND_LIBRARY(Readline_LIBRARY NAMES readline)
+else(READLINE_INCLUDE_DIR AND READLINE_LIBRARY AND NCURSES_LIBRARY)
+    FIND_LIBRARY(READLINE_LIBRARY NAMES readline)
     include(FindPackageHandleStandardArgs)
-    FIND_PACKAGE_HANDLE_STANDARD_ARGS(Readline DEFAULT_MSG Readline_INCLUDE_DIR Readline_LIBRARY)
-    MARK_AS_ADVANCED(Readline_INCLUDE_DIR Readline_LIBRARY)
-endif(Readline_INCLUDE_DIR AND Readline_LIBRARY AND Ncurses_LIBRARY)
+    FIND_PACKAGE_HANDLE_STANDARD_ARGS(Readline DEFAULT_MSG READLINE_INCLUDE_DIR READLINE_LIBRARY)
+    MARK_AS_ADVANCED(READLINE_INCLUDE_DIR READLINE_LIBRARY)
+endif(READLINE_INCLUDE_DIR AND READLINE_LIBRARY AND NCURSES_LIBRARY)
 
 # Hide these variables in cmake GUIs
-mark_as_advanced(Readline_ROOT_DIR Readline_INCLUDE_DIR Readline_LIBRARY)
+mark_as_advanced(READLINE_ROOT_DIR READLINE_INCLUDE_DIR READLINE_LIBRARY)
