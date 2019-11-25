8b1d0df7935b2d6e92502010862676a8ee47693c
Filipe <filipefalcaosantos@gmail.com>
2019-11-25 10:06:03 -0300

    [fix] Fixes freeing closure objects.
    Changes the type of the upvalue array from "ObjUpvalue" to "ObjUpvalue *", since it is allocated as the latter.

--- src/vm/yapl_memmanager.c
+++ src/vm/yapl_memmanager.c
@@ -63,7 +63,7 @@ static void freeObject(Obj *object) {
             break;
         case OBJ_CLOSURE: {
             ObjClosure *closure = (ObjClosure *) object;
-            FREE_ARRAY(ObjUpvalue, closure->upvalues, closure->upvalueCount);
+            FREE_ARRAY(ObjUpvalue *, closure->upvalues, closure->upvalueCount);
             FREE(ObjClosure, object);
             break;
         }
